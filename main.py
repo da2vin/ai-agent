3f0ef08db9929ae3e4fb8f503be029e96c03367c
da2vin <darkwings_love@163.com>
2025-07-15 14:15:27 +0800

    [chore] opimized

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import asyncio
 import datetime
 from dotenv import load_dotenv
 from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import SystemMessage, HumanMessage
 
 load_dotenv(verbose=True)
-
-llm = ChatOpenAI(model="gpt-4o-mini")
 
 
 @tool
@@ -20,14 +20,26 @@ def get_current_date():
     return datetime.datetime.now().strftime("%Y-%m-%d")
 
 
+llm = ChatOpenAI(model="gpt-4o-mini")
+
 agent = create_react_agent(
     model=llm,
     tools=[get_current_date],
     prompt="You are a helpful assistant.",
 )
 
-result = agent.invoke({"messages": [
-    {"role": "user", "content": "今天是几月几号？"}
-]})
 
-print(result)
+async def invoke():
+    messages = [
+        SystemMessage("你是一个私人助手"),
+        HumanMessage("今天是几月几号？"),
+    ]
+
+    result = await agent.ainvoke({
+        "messages": messages
+    })
+    print(result)
+
+
+if __name__ == '__main__':
+    asyncio.run(invoke())
